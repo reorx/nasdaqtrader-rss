6611407dc0737f17acc26dd0a77478969bdc63df
Reorx <novoreorx@gmail.com>
2019-07-15 17:15:28 +0800

    update index html

--- index.py
+++ index.py
@@ -1,10 +1,30 @@
-from flask import Flask, Response, __version__
+from flask import Flask, Response
 
 
 app = Flask(__name__)
+
+tpl = """\
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Nasdaqtrader RSS</title>
+    </head>
+    <body>
+        <h1>Nasdaqtrader RSS</h1>
+        <p>Transform Nasdaqtrader news html page to rss API
+        <br/>
+        RSS List:
+        </p>
+        <ul>
+            <li>News - Corporate Action Alerts: <a href="/rss">/rss</a>
+            (<a href="https://www.nasdaqtrader.com/Trader.aspx?id=archiveheadlines&cat_id=105">source</a>)</li>
+        </ul>
+    </body>
+</html>
+"""
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response('<h1>Flask on Now 2.0 {}</h1>'.format(__version__), mimetype='text/html')
+    return Response(tpl, mimetype='text/html')
--- rss/index.py
+++ rss/index.py
@@ -14,12 +14,13 @@ css = '<link rel="stylesheet" href="/css/style.css" />'
 curl 'https://www.nasdaqtrader.com/RPCHandler.axd' -H 'Origin: https://www.nasdaqtrader.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36' -H 'Content-type: application/json' -H 'Accept: */*' -H 'Referer: https://www.nasdaqtrader.com/Trader.aspx?id=archiveheadlines&cat_id=105' -H 'Cookie: ASP.NET_SessionId=3go24gkjl30e4s3aehunangp' -H 'Connection: keep-alive' --data-binary '{"id":2,"method":"BL_NewsListing.GetArchiveHeadlinesPageData","params":"[\"105\",2019,0]","version":"1.1"}' --compressed
 """
 
+source_url = 'https://www.nasdaqtrader.com/Trader.aspx?id=archiveheadlines&cat_id=105'
 url = 'https://www.nasdaqtrader.com/RPCHandler.axd'
 headers = {
     'Content-type': 'application/json',
     'Host': 'www.nasdaqtrader.com',
     'Origin': 'https://www.nasdaqtrader.com',
-    'Referer': 'https://www.nasdaqtrader.com/Trader.aspx?id=archiveheadlines&cat_id=105',
+    'Referer': source_url,
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
 }
 req_data = {
@@ -49,7 +50,7 @@ def parse_html_to_feed(html):
     fg.title('Nasdaqtrader Corporate Actions')
     fg.description('Nasdaqtrader Corporate Actions')
     fg.link(dict(
-        href='https://github.com/reorx',
+        href=source_url,
         rel='alternate',
     ))
 
